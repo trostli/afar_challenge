7c3f4b678866586456ac075fe7099ceb30396076
Daniel Trostli <daniel.trostli@gmail.com>
2013-11-27 13:32:46 -0800

    Created new rails 3.2.13 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AFARBackend::Application.config.secret_token = '7082a0c304d8da0fa8a29113a663c1af54529feabe285e975fa87baa003f51dc14aae887111cbdc26adce06cca1b039703638108f086bafe064d1a92ee8975d6'
