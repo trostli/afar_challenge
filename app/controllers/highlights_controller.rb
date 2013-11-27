b670d0a7ce77df58a557345071312d114b7f20b1
Daniel Trostli <daniel.trostli@gmail.com>
2013-11-27 14:50:31 -0800

    Added form partial and controller methods

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,3 +1,15 @@
 class HighlightsController < ApplicationController
 
+  def index
+    @highlights = Highlight.all
+    @highlight = Highlight.new
+  end
+
+  def create
+    @highlight = Highlight.new
+    @highlight.title = params[:highlight][:title]
+    @highlight.description = params[:highlight][:description]
+    @highlight.save
+    User.find(current_user.id).highlights << @highlight
+  end
 end
