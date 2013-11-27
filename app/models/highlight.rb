a30e7cd9d5cc2ebbf4edf4ac70865b1e0623e9c4
Daniel Trostli <daniel.trostli@gmail.com>
2013-11-27 14:52:48 -0800

    Added associations to models

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -1,3 +1,6 @@
 class Highlight < ActiveRecord::Base
-
+  attr_accessible :title, :description, :user_id
+  validates :title, presence: true
+  validates :description, presence: true
+  belongs_to :user
 end
